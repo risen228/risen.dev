8f1a80a9d56edf41f27c7bfea26fb8691e481604
risenforces <to@risen.dev>
2020-01-29 02:47:33 +0500

    localization

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,11 +1,26 @@
-const formatters = {
-  fullDate: new Intl.DateTimeFormat('ru-RU', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-  }),
+const monthMap = {
+  0: 'января',
+  1: 'февраля',
+  2: 'марта',
+  3: 'апреля',
+  4: 'мая',
+  5: 'июня',
+  6: 'июля',
+  7: 'августа',
+  8: 'сентября',
+  9: 'октября',
+  10: 'ноября',
+  11: 'декабря',
 }
 
 export const fullDate = date => {
-  return formatters.fullDate.format(new Date(date))
+  const d = new Date(date)
+
+  const day = d.getDate()
+  const month = d.getMonth()
+  const year = d.getFullYear()
+
+  const monthLong = monthMap[month]
+
+  return `${day} ${monthLong} ${year} г.`
 }
