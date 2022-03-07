b824ddc28e21f6c02b00d0393bacc32ff8fb2db4
Evgeny Zakharov <risenforces@gmail.com>
2022-03-08 02:39:15 +0300

    feat: add copy button

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,14 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-code-buttons',
+            options: {
+              svgIcon: `<svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path xmlns="http://www.w3.org/2000/svg" d="M2 4C2 2.89543 2.89543 2 4 2H14C15.1046 2 16 2.89543 16 4V8H20C21.1046 8 22 8.89543 22 10V20C22 21.1046 21.1046 22 20 22H10C8.89543 22 8 21.1046 8 20V16H4C2.89543 16 2 15.1046 2 14V4ZM10 16V20H20V10H16V14C16 15.1046 15.1046 16 14 16H10ZM14 14V4L4 4V14H14Z" fill="rgba(255, 255, 255, 0.65)"></path>
+              </svg>`,
+            },
+          },
+          {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
@@ -52,6 +60,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-prismjs',
             options: {
+              copy: true,
               inlineCodeMarker: '÷',
               languageExtensions: [
                 {
